fa77fbaaca1d2746717c0556448a10ecc82a8668
KonstantinS <sherbakov.konst@yandex.ru>
2020-11-18 10:22:17 +0700

    Немного коментария добавил, чтобы понять что @Autowired может быть явно указан, а не в конструкторе

--- src/main/kotlin/com/example/demo/controller/BuyController.kt
+++ src/main/kotlin/com/example/demo/controller/BuyController.kt
@@ -2,13 +2,18 @@ package com.example.demo.controller
 
 import com.example.demo.domain.Buy
 import com.example.demo.service.BuyService
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/buy")
 class BuyController(
-        private val buyService: BuyService
+//        private val buyService: BuyService
 ) {
+//    Вместо объявления в конструкторе, объявил тут
+    @Autowired
+    private lateinit var buyService: BuyService
+
     @GetMapping("/getAll")
     fun getAllBuys(): List<Buy> {
         return buyService.getAll()
